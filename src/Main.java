88b8d4a7d40d47ecced5bf91f5afd545541d9117
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-07-29 09:50:16 -0300

    Adicionando exemplo com listas

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        pessoas.add(new Pessoa("111.111.111-01", "João"));
+        pessoas.add(new Pessoa("222.222.222-02", "Maria"));
+        pessoas.add(0, new Pessoa("333.333.333-03", "Pedro"));
+        pessoas.add(3, new Pessoa("444.444.444-04", "Ana"));
+
+        System.out.println(pessoas);
+
     }
 }
