9e89d799e37a19f2aac935af3c1158cfe1f3e8fd
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-07-31 09:05:13 -0300

    Fazendo exemplo de uso da fila

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        Fila fila = new Fila();
 
+        fila.adicionar(new Ficha());
+        fila.adicionar(new Ficha());
+        fila.adicionar(new Ficha());
+        fila.adicionar(new Ficha());
+        fila.adicionar(new Ficha());
+
+        System.out.println(fila.verProximo());
+
+        System.out.println(fila.chamarProximo());
+        System.out.println(fila.chamarProximo());
+        System.out.println(fila.chamarProximo());
 
     }
 }
